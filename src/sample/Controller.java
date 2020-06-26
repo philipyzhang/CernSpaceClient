f1a8763326b20a28f4979838b8f3b352b20c4da3
Liang Liu <stuffbyliang@gmail.com>
2020-06-26 12:54:54 -0700

    fixed merge conflict and ui wrong dimensions

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,11 +1,10 @@
 package sample;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
-
-import java.awt.event.ActionEvent;
 
 public class Controller {
 
