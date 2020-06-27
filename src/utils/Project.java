a5fbdda2d545bee0e0dca1116128c1970dc0a8d1
Devam Sisodraker <devam@sisodraker.com>
2020-06-27 09:21:36 -0700

    Only used minimum fields for the class

---
+++ src/utils/Project.java
@@ -0,0 +1,16 @@
+package utils;
+
+public class Project {
+
+    public String _id = "";
+    public String name = "";
+    public String hostIp = "";
+    public Integer hostPort = 0;
+    public String hostToken = "";
+    public Integer lastUpdated = 0;
+
+    public Project() {
+
+    }
+
+}
